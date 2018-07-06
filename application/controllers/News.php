b49acd2cf5a633105bf1a9c4accba914e54b95b2
NourAlhadi <nour_alhadi9@yahoo.com>
2018-07-06 17:35:27 +0300

    Initial Commit

---
+++ application/controllers/News.php
@@ -0,0 +1,10 @@
+<?php
+
+class News extends CI_Controller{
+
+    public function index (){
+        $this->load->view('index');
+    }
+
+
+}
---
+++ application/views/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Index Page</title>
+</head>
+<body>
+    <h1 style="text-align: center; color:wheat;">Hello from CodeIgniter</h1>
+</body>
+</html>
